0cb07cb31958a812d1481322df3134f285894bb5
vantagedevasha2022@gmail.com <vantagedevasha2022@gmail.com>
2022-07-26 13:48:26 +0000

    added reporting

--- models/raw/raw_customer.sql
+++ models/raw/raw_customer.sql
@@ -1,8 +1,8 @@
 {{
     config(
-        materialized='table'
+        materialized='view'
     )
 }}
 
 select * from
-"RAW"."GLOBALMART"."CUSTOMER"
+{{ source('globalmart', 'customer') }}
--- models/raw/raw_orders.sql
+++ models/raw/raw_orders.sql
@@ -5,15 +5,4 @@
 }}
 
 select * from
-"RAW"."GLOBALMART"."ORDER"
-
---create two more tables, remember to add a .sql when creating the model
---raw_product 
---select * from
---"RAW"."GLOBALMART"."PRODUCT"
-
---raw_customer
---select * from
---"RAW"."GLOBALMART"."CUSTOMER"
-
---dbt run and commit
+{{ source('globalmart', 'orders') }}
